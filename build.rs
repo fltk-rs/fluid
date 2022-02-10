0124b91f96f4b8f124daa84a7f9c167e363af99f
MoAlyousef <may642_2000@hotmail.com>
2022-02-10 21:46:04 +0300

    use latest fltk

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ use std::{env, path::PathBuf, process::Command};
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let cargo_home = PathBuf::from(env::var("CARGO_HOME").unwrap());
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
 
     println!("cargo:rerun-if-changed=build.rs");
@@ -25,94 +26,19 @@ fn main() {
         .define("OPTION_LARGE_FILE", "ON")
         .define("OPTION_BUILD_HTML_DOCUMENTATION", "OFF")
         .define("OPTION_BUILD_PDF_DOCUMENTATION", "OFF");
-
     dst.build();
 
-    println!(
-        "cargo:rustc-link-search=native={}",
-        out_dir.join("build").display()
-    );
+    let mut inp = out_dir.join("bin/fluid");
 
-    println!(
-        "cargo:rustc-link-search=native={}",
-        out_dir.join("build").join("Release").display()
-    );
-
-    println!(
-        "cargo:rustc-link-search=native={}",
-        out_dir.join("build").join("lib").display()
-    );
-
-    println!(
-        "cargo:rustc-link-search=native={}",
-        out_dir.join("build").join("lib").join("Release").display()
-    );
-
-    println!(
-        "cargo:rustc-link-search=native={}",
-        out_dir.join("lib").display()
-    );
-
-    println!(
-        "cargo:rustc-link-search=native={}",
-        out_dir.join("lib64").display()
-    );
-
-    println!(
-        "cargo:rustc-link-search=native={}",
-        out_dir.join("lib").join("Release").display()
-    );
-
-    println!(
-        "cargo:rustc-link-search=native={}",
-        out_dir.join("lib64").join("Release").display()
-    );
-
-    println!("cargo:rustc-link-lib=static=fltk");
-    println!("cargo:rustc-link-lib=static=fltk_images");
-    println!("cargo:rustc-link-lib=static=fltk_z");
-    println!("cargo:rustc-link-lib=static=fltk_jpeg");
-    println!("cargo:rustc-link-lib=static=fltk_png");
-    println!("cargo:rustc-link-lib=static=fltk_forms");
-    println!("cargo:rustc-link-lib=static=fluid");
+    let mut out = cargo_home.join("bin/fltk-fluid");
 
     match target_os.as_str() {
-        "macos" => {
-            println!("cargo:rustc-link-lib=framework=Carbon");
-            println!("cargo:rustc-link-lib=framework=Cocoa");
-            println!("cargo:rustc-link-lib=framework=ApplicationServices");
-            println!("cargo:rustc-link-lib=dylib=c++");
-        }
         "windows" => {
-            println!("cargo:rustc-link-lib=dylib=gdiplus");
-            println!("cargo:rustc-link-lib=dylib=ws2_32");
-            println!("cargo:rustc-link-lib=dylib=comctl32");
-            println!("cargo:rustc-link-lib=dylib=gdi32");
-            println!("cargo:rustc-link-lib=dylib=oleaut32");
-            println!("cargo:rustc-link-lib=dylib=ole32");
-            println!("cargo:rustc-link-lib=dylib=uuid");
-            println!("cargo:rustc-link-lib=dylib=shell32");
-            println!("cargo:rustc-link-lib=dylib=advapi32");
-            println!("cargo:rustc-link-lib=dylib=comdlg32");
-            println!("cargo:rustc-link-lib=dylib=winspool");
-            println!("cargo:rustc-link-lib=dylib=user32");
-            println!("cargo:rustc-link-lib=dylib=kernel32");
-            println!("cargo:rustc-link-lib=dylib=odbc32");
-            if cfg!(target_env = "gnu") {
-                println!("cargo:rustc-link-lib=dylib=stdc++");
-            }
+            inp = inp.join(".exe");
+            out = out.join(".exe");
         }
-        _ => {
-            println!("cargo:rustc-link-lib=dylib=pthread");
-            println!("cargo:rustc-link-lib=dylib=X11");
-            println!("cargo:rustc-link-lib=dylib=Xext");
-            println!("cargo:rustc-link-lib=dylib=Xinerama");
-            println!("cargo:rustc-link-lib=dylib=Xcursor");
-            println!("cargo:rustc-link-lib=dylib=Xrender");
-            println!("cargo:rustc-link-lib=dylib=Xfixes");
-            println!("cargo:rustc-link-lib=dylib=Xft");
-            println!("cargo:rustc-link-lib=dylib=fontconfig");
-            println!("cargo:rustc-link-lib=dylib=stdc++");
-        }
+        _ => (),
     }
+
+    std::fs::copy(&inp, &out).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,12 @@
-//! # fltk-rs-fluid
-//! 
-//! A Rust crate wrapping FLTK's fluid (RAD tool).
-//! 
-//! It allows installing fluid via cargo-install:
-//! 
-//! ```ignored
-//! $ cargo install fltk-fluid
-//! ```
-//! 
+// ! # fltk-rs-fluid
+// ! 
+// ! A Rust crate wrapping FLTK's fluid (RAD tool).
+// ! 
+// ! It allows installing fluid via cargo-install:
+// ! 
+// ! ```ignored
+// ! $ cargo install fltk-fluid
+// ! ```
+// ! 
 
-use std::ffi::CString;
-use std::{env, os::raw};
-
-extern "C" {
-    pub fn fluid_main(argc: raw::c_int, argv: *mut *mut raw::c_char) -> raw::c_int;
-}
-
-fn main() {
-    let mut args: Vec<_> = env::args()
-        .into_iter()
-        .map(|s| CString::new(s).unwrap().into_raw())
-        .collect();
-    args.push(std::ptr::null_mut());
-    unsafe {
-        fluid_main(args.len() as i32 -1, args.as_mut_ptr());
-    }
-}
+fn main() {}
